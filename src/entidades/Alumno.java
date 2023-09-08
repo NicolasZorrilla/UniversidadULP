7f8397cc7417c9edb7ae14575444d5de659a6341
Nicolás Zorrilla <134982268+NicolasZorrilla@users.noreply.github.com>
2023-09-08 19:50:31 -0300

    Clases en Entidades completadas
    Se completaron el código de las tres clases del paquete Entidades.

--- src/entidades/Alumno.java
+++ src/entidades/Alumno.java
@@ -1,14 +1,79 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package entidades;
 
-/**
- *
- * @author nicolas
- */
+import java.time.LocalDate;
+
 public class Alumno {
+    
+    private int idAlumno;
+    private String apellido;
+    private String nombre;
+    private LocalDate fechaNacimiento;
+    private boolean estado;
+    
+    public Alumno() {} // Constructor vacio.
+
+    // Constructor completo.
+    public Alumno(int idAlumno, String apellido, String nombre, LocalDate fechaNacimiento, boolean estado) {
+        this.idAlumno = idAlumno;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.estado = estado;
+    }
+    
+    // Constructor sin el atributo idAlumno.
+    public Alumno(String apellido, String nombre, LocalDate fechaNacimiento, boolean estado) {
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.estado = estado;
+    }
+
+    // Metodos Getter and Setter:
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "idAlumno=" + idAlumno + ", apellido=" + apellido + ", nombre=" + nombre + ", fechaNacimiento=" + fechaNacimiento + ", estado=" + estado + '}';
+    }
     
 }
--- src/entidades/Inscripcion.java
+++ src/entidades/Inscripcion.java
@@ -1,14 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package entidades;
 
-/**
- *
- * @author nicolas
- */
 public class Inscripcion {
+    
+    private int idInscripcion;
+    private Alumno alumno;
+    private Materia materia;
+    private int nota;
+    
+    public Inscripcion() {} // Constructor vacio.
+
+    // Constructor completo.
+    public Inscripcion(int idInscripcion, Alumno alumno, Materia materia, int nota) {
+        this.idInscripcion = idInscripcion;
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    // Constructor sin el atributo idInscripcion.
+    public Inscripcion(Alumno alumno, Materia materia, int nota) {
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    // Metodos Getter and Setter.
+    public int getIdInscripcion() {
+        return idInscripcion;
+    }
+
+    public void setIdInscripcion(int idInscripcion) {
+        this.idInscripcion = idInscripcion;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
     
 }
