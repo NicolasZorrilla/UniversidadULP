a06e631f9526e8acfd2434fc5793c4bc45f60b36
chanuar santiago <y.ussef@hotmail.com>
2023-09-08 22:04:43 -0300

    cambios ins

--- src/accesoadatos/InscripcionData.java
+++ src/accesoadatos/InscripcionData.java
@@ -5,10 +5,105 @@
  */
 package accesoadatos;
 
+import entidades.Inscripcion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author nicolas
  */
 public class InscripcionData {
     
+    private Connection con = null;
+    private MateriaData materiadata;
+    private AlumnoData alumnosdata;
+    
+    
+
+    public InscripcionData() {
+        
+        con= Conexion.getConexion();
+    }
+    
+    
+    
+    
+    
+    
+    
+    public void guardarInscripcion (Inscripcion Inscripcion) {
+        
+        String sql = "INSERT INTO Inscripcion (Alumno, Materia ,nota)"
+                +"VALUES (?, ?, ?)";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            ps.setObject(1, Inscripcion.getAlumno());
+            ps.setObject(2, Inscripcion.getMateria());
+            ps.setInt(3, Inscripcion.getNota());
+            
+            ResultSet RS = ps.getGeneratedKeys();//tabla de inscripcion
+            
+            
+             if (RS.next()) {
+             
+             Inscripcion.setIdInscripcion(RS.getInt(1));
+             
+             
+               JOptionPane.showMessageDialog(null,"inscripcion guardado");
+             } 
+            ps.close();
+           
+            
+        } catch (SQLException ex) {
+             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla de inscripcion"+ex.getMessage());
+        }
+        
+        
+    }
+    
+  public List<Inscripcion> obtenerInscripcion() {
+        
+       
+    
+    List<Inscripcion> Inscripcion = new ArrayList<>();
+  
+    
+ try{   
+    
+    String sql = "SELECT * FROM Inscripion WHERE Alumno"; 
+    
+    PreparedStatement ps = con.prepareStatement(sql);
+    ResultSet rs = ps.executeQuery();
+      
+  while (rs.next()) {
+      
+      Inscripcion inscripcion = new Inscripcion();
+      
+      
+      
+      
+      
+      
+      
+  }
+  
+   
+ } catch (SQLException ex) {
+          JOptionPane.showMessageDialog(null, " "+ex.getMessage());
+    
+ }   
+   return Inscripcion; 
+}
+  
 }
