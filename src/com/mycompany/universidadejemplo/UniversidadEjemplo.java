2d022307de6783e21c41a37813945ffee7e80b46
Nicolás Zorrilla <134982268+NicolasZorrilla@users.noreply.github.com>
2023-09-24 01:36:12 -0300

    Cambio pequeño

--- src/com/mycompany/universidadejemplo/UniversidadEjemplo.java
+++ src/com/mycompany/universidadejemplo/UniversidadEjemplo.java
@@ -6,8 +6,6 @@ import gui.*;
 public class UniversidadEjemplo {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
         MenuView desktopView = new MenuView();
         desktopView.setVisible(true);
         desktopView.setLocationRelativeTo(null);
