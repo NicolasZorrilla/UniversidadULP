99201df673acc259b519b0f3dbd9e58509449afe
Nicolás Zorrilla <134982268+NicolasZorrilla@users.noreply.github.com>
2023-09-25 20:08:05 -0300

    Se renombro el proyecto

--- src/dataBaseAccess/DataBaseConnection.java
+++ src/dataBaseAccess/DataBaseConnection.java
@@ -6,7 +6,7 @@ import javax.swing.JOptionPane;
 
 public class DataBaseConnection {
     private static final String URL = "jdbc:mariadb://localhost/";
-    private static final String DB = "UniversidadULP24";
+    private static final String DB = "UniversidadULP";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "";
     private static Connection connection;
